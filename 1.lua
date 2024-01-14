12f507ccf9c1962ded0ca34d351a90bdf8f93c9f
Miguel Fernández <miguel.fernandez008@gmail.com>
2024-01-14 17:17:48 +0100

    :sparkles: Trebuchet?! - Day 1
    :books: Change log
    :new: Moved part 2 to lua.
    :new: Helper functions.

--- 1.lua
+++ 1.lua
@@ -1,5 +1,45 @@
 require 'utils.functions'
 
+function find_number_as_text(char_buffer)
+    local valid_string_as_number_values = {
+        "one", 
+        "two", 
+        "three", 
+        "four", 
+        "five", 
+        "six", 
+        "seven", 
+        "eight", 
+        "nine"
+    }
+    local found = 0
+    for k, v in pairs(valid_string_as_number_values) do
+        if string.find(char_buffer, v) then
+            found = k
+            break
+        end
+    end
+    return found
+end
+
+function store_digit(digits, digist_found, value)
+    if digist_found == 0 then
+        digits[1] = value
+    else
+        digits[2] = value
+    end
+end
+
+function mirror_first_digit(digits, digist_found)
+    if digist_found == 1 and digits[2] == nil then
+        digits[2] = digits[1]
+    end       
+end
+
+function get_line_total(digits)
+    return tonumber(table.concat(digits))
+end
+
 function part1()
    Utils:open("1")
    local total_sum = 0   
@@ -12,18 +52,12 @@ function part1()
         for i = 1, #line do
             local char = string.sub(line, i, i)
             if tonumber(char) then
-                if digist_found == 0 then
-                    digits[1] = char
-                else
-                    digits[2] = char
-                end
+                store_digit(digits, digist_found, char)
                 digist_found = digist_found + 1
             end
         end   
-        if digist_found == 1 and digits[2] == nil then
-            digits[2] = digits[1]
-        end       
-        local total_line = tonumber(table.concat(digits))
+        mirror_first_digit(digits, digist_found)
+        local total_line = get_line_total(digits)
         total_sum = total_sum + total_line
     end    
    end
@@ -34,6 +68,34 @@ end
 function part2()
     Utils:open("1")
     local total_sum = 0
+    if Utils.file then
+        local lines = Utils:lines()
+        for key, line in pairs(lines) do
+            local digits = {}
+            local length = #lines
+            local digist_found = 0
+            local char_buffer = ""        
+            for i = 1, #line do
+                local char = string.sub(line, i, i)
+                if tonumber(char) then
+                    store_digit(digits, digist_found, char)
+                    digist_found = digist_found + 1
+                    char_buffer = ""
+                else
+                    char_buffer = char_buffer .. char
+                    local key = find_number_as_text(char_buffer)
+                    if key ~= 0 then
+                        store_digit(digits, digist_found, key)
+                        digist_found = digist_found + 1
+                        char_buffer = string.sub(char_buffer, -1)
+                    end
+                end
+            end
+            mirror_first_digit(digits, digist_found)
+            local total_line = get_line_total(digits)
+            total_sum = total_sum + total_line
+        end
+    end
     Utils:close()
     print(string.format("Total part 2 is: %d", total_sum))
 end
